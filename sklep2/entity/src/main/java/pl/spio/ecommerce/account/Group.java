2076721defdb2eba45546e731a2610060de1b21c
spio11 <spio11@LAB-GE-7.CSPUT>
2014-05-31 14:59:23 +0200

    2014.05.31 commit 3

--- sklep2/entity/src/main/java/pl/spio/ecommerce/account/Group.java
+++ sklep2/entity/src/main/java/pl/spio/ecommerce/account/Group.java
@@ -13,23 +13,27 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "GROUPS", schema = "public")
 public class Group implements Serializable {
-@Id
-@SequenceGenerator(name = "grpSeqGen", sequenceName = "GRP_ID_SEQ")
-@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "grpSeqGen")
-@Column(name = "GRP_ID", unique = true, nullable = false)
-private long id;
-@Column(name = "GRP_GROUPNAME", length = 64, nullable = false)
-private String groupname;
-public long getId() {
-	return id;
-}
-public void setId(long id) {
-	this.id = id;
-}
-public String getGroupname() {
-	return groupname;
-}
-public void setGroupname(String groupname) {
-	this.groupname = groupname;
-}
+	@Id
+	@SequenceGenerator(name = "grpSeqGen", sequenceName = "GRP_ID_SEQ")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "grpSeqGen")
+	@Column(name = "GRP_ID", unique = true, nullable = false)
+	private long id;
+	@Column(name = "GRP_GROUPNAME", length = 64, nullable = false)
+	private String groupname;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getGroupname() {
+		return groupname;
+	}
+
+	public void setGroupname(String groupname) {
+		this.groupname = groupname;
+	}
 }
